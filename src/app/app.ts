fd3fe9ce94db8c8a4b1077aca52c415bfdc1ea7d
yuriiivanchuk <yurii.ivanchuk@wyldnetworks.com>
2025-12-23 17:25:41 +0100

    implement clock

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,77 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  OnDestroy,
+  OnInit,
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  DOCUMENT,
+  inject,
+  signal,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class App {
-  protected readonly title = signal('clock-and-weather-widget');
+export class App implements OnInit, OnDestroy {
+  private readonly document = inject(DOCUMENT);
+  private frameId = 0;
+
+  readonly smallMarkers = this.createMarkers(60);
+  readonly mediumMarkers = this.createMarkers(12);
+  readonly largeMarkers = this.createMarkers(4);
+
+  now = signal(new Date());
+
+  date = computed(() => {
+    const now = this.now();
+    const weekday = now.toLocaleDateString(undefined, { weekday: 'short' });
+    const date = now.toLocaleDateString(undefined, {
+      year: 'numeric',
+      month: 'short',
+    });
+
+    return `${weekday}, ${date}`;
+  });
+
+  secondDeg = computed(() => {
+    const now = this.now();
+    const seconds = now.getSeconds() + now.getMilliseconds() / 1000;
+    return seconds * 6;
+  });
+
+  minuteDeg = computed(() => {
+    const now = this.now();
+    const minutes = now.getMinutes() + now.getSeconds() / 60;
+    return minutes * 6;
+  });
+
+  hourDeg = computed(() => {
+    const now = this.now();
+    const hours = (now.getHours() % 12) + now.getMinutes() / 60;
+    return hours * 30;
+  });
+
+  ngOnInit() {
+    this.updateNow();
+  }
+
+  ngOnDestroy() {
+    this.document.defaultView?.cancelAnimationFrame(this.frameId);
+  }
+
+  private createMarkers(count: number): number[] {
+    return Array.from({ length: count }, (_, i) => (360 / count) * i);
+  }
+
+  private updateNow() {
+    if (!this.document.defaultView) return;
+
+    this.now.set(new Date());
+    this.frameId = this.document.defaultView.requestAnimationFrame(() => this.updateNow());
+  }
 }
